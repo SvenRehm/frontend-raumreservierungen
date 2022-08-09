d02b12efefd06a7750da836c5ddb196788fbda63
SvenRehm <rehm21@web.de>
2022-08-09 17:11:52 +0200

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import HomeComponents from "@/components/HomeComponents.vue"
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+   <main>
+      <HomeComponents />
+   </main>
 </template>
